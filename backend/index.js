c45d90ffffcc9d8fdf533735995d04e200253488
andholemoksha <andholemoksha@gmail.com>
2025-10-06 14:26:35 +0530

    final-changes-1

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*" ,// React frontend
+    origin: ["https://livepoll-frontend-osvf.onrender.com","https://iridescent-yeot-138a8f.netlify.app/"] ,// React frontend
     methods: ["GET", "POST"]
   }
 });
--- frontend/src/server/socket.js
+++ frontend/src/server/socket.js
@@ -1,7 +1,7 @@
 // src/socket.ts
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:3000"; // your backend server
+const SOCKET_URL = process.env.SOCKET_URL ||"https://livepoll-n1df.onrender.com"|| "http://localhost:3000"; // your backend server
 
 // Create a singleton socket instance
 const socket = io(SOCKET_URL, {
